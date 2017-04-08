ac4a633bd99ebb8106974fc2e567eaa26a7fbe82
ireullin <cheinheng.lin@uitox.com>
2017-04-08 21:13:46 +0800

    Set method to private

--- lib/kmeans-crystal.rb
+++ lib/kmeans-crystal.rb
@@ -38,43 +38,18 @@ end
 
 
 class Model
-
-    def kmeans_pp(entries, cluster_num)
-        features = entries.map{|x| x[@vector_name]}
-        dimension = features.first.size
-        init_val = Array.new(dimension){[0.0,0.0]}
-        dimension.times do |i|
-            init_val[i][0] = features.map{|f| f[i]}.to_a.min
-            init_val[i][1] = features.map{|f| f[i]}.to_a.max
-        end
-
-        init_centroids = entries.sample(cluster_num).map{|x| x[@vector_name]}
-        combination = 2**dimension
-        combination.times do |i|
-            offset = i.to_s(2).rjust(dimension,"0")
-            dimension.times do |d|
-                next if i >= init_centroids.size
-                max_or_min = offset[d].to_i
-                init_centroids[i][d] = init_val[d][max_or_min]
-            end
-        end
-
-        return init_centroids
-    end
-
-
-    def initialize(cluster_num, entries, kmeans_pp = true, vector_name = :features)
+    def initialize(cluster_num, entries, ues_kmeans_pp = true, vector_name = :features)
         raise 'too less cluster_num to evaluate k-means' if entries.size < cluster_num
         @cluster_num = cluster_num
         @entries = entries
         @vector_name = vector_name
 
-        init_centroids = if kmeans_pp
+        init_centroids = if ues_kmeans_pp
             kmeans_pp(@entries, @cluster_num)
         else
             @entries.sample(@cluster_num).map{|x| x[@vector_name]}
         end
-
+        puts "init_centroids=#{init_centroids.inspect}"
         @clusters = new_clusters(init_centroids)
     end
 
@@ -138,6 +113,29 @@ class Model
             arr << Cluster.new(cluster.name, cluster.update_centroid, @vector_name)
         end
         return arr
+    end
+
+    def kmeans_pp(entries, cluster_num)
+        features = entries.map{|x| x[@vector_name]}
+        dimension = features.first.size
+        init_val = Array.new(dimension){[0.0,0.0]}
+        dimension.times do |i|
+            init_val[i][0] = features.map{|f| f[i]}.to_a.min
+            init_val[i][1] = features.map{|f| f[i]}.to_a.max
+        end
+
+        init_centroids = entries.sample(cluster_num).map{|x| x[@vector_name]}
+        combination = 2**dimension
+        combination.times do |i|
+            offset = i.to_s(2).rjust(dimension,"0")
+            dimension.times do |d|
+                next if i >= init_centroids.size
+                max_or_min = offset[d].to_i
+                init_centroids[i][d] = init_val[d][max_or_min]
+            end
+        end
+
+        return init_centroids
     end
 end
 end
